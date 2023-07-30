eca157e354eb60a2f40250b5888148c561a9b031
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-07-30 12:46:38 -0300

    Comentando novas anotações

--- src/main/java/io/github/WesleyViricimo/Main.java
+++ src/main/java/io/github/WesleyViricimo/Main.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"io.github.WesleyViricimo.repository", "io.github.WesleyViricimo.repository"}) //Anotação para indicar para o spring quais são os componentes, classes de configurações e objetos que deverão ser escaneados, mas o spring já escaneia todos os pacotes que estão no pacote principal e é mais utilizada esta anotação quando utilizadas bibliotecas de terceiros
 @SpringBootApplication //Anotação para Spring reconhecer que a classe irá inicializar uma aplicação Spring Boot
 public class Main {
 
---
+++ src/main/java/repository/MyRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository //Indicando para o sprint que a classe irá acessar a base de dados
+public class MyRepository {
+}

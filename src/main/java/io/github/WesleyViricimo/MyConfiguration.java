1a4fc4619049bfb1d58a1c51261b917c1bed89b2
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-07-31 16:12:53 -0300

    Anotações customizadas

--- src/main/java/io/github/WesleyViricimo/MyConfiguration.java
+++ src/main/java/io/github/WesleyViricimo/MyConfiguration.java
@@ -1,15 +1,13 @@
 package io.github.WesleyViricimo;
 
+import io.github.WesleyViricimo.profile.Development;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("development")
+@Development
 public class MyConfiguration {
 
-    @Bean(name = "applicationName") //Anotação @Bean cria o método no contexto da aplicação e poderá ser utilizado em qualquer parte da aplicação e se não for passado um nome para o bean o nome será o próprio nome do método;
+    @Bean //Anotação @Bean cria o método no contexto da aplicação e poderá ser utilizado em qualquer parte da aplicação e se não for passado um nome para o bean o nome será o próprio nome do método;
     public CommandLineRunner executar(){
         return args -> {
             System.out.println("Executando a configuração de desenvolvimento");

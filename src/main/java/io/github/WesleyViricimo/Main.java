e1913afa85275d51d38f57565897961ccb9f4681
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-07-31 16:20:03 -0300

    Limpando aplicação

--- src/main/java/io/github/WesleyViricimo/Main.java
+++ src/main/java/io/github/WesleyViricimo/Main.java
@@ -1,27 +1,11 @@
 package io.github.WesleyViricimo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
 
 
-@ComponentScan(basePackages = {"io.github.WesleyViricimo.repository", "io.github.WesleyViricimo.repository"}) //Anotação para indicar para o spring quais são os componentes, classes de configurações e objetos que deverão ser escaneados, mas o spring já escaneia todos os pacotes que estão no pacote principal e é mais utilizada esta anotação quando utilizadas bibliotecas de terceiros
 @SpringBootApplication //Anotação para Spring reconhecer que a classe irá inicializar uma aplicação Spring Boot
 public class Main {
-
-    /*@Autowired
-    @Qualifier("applicationName") //Anotação Qualifier é utilizada para identificar o Bean que será injetado na variável
-     */
-    @Value("${application.name}")
-    private String applicationName;
-
-
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return applicationName;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);

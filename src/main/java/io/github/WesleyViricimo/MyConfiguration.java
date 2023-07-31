b337a6694e179f0a67afaab7008326332b5c2419
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-07-30 22:24:01 -0300

    Comentário novas anotações

--- src/main/java/io/github/WesleyViricimo/MyConfiguration.java
+++ src/main/java/io/github/WesleyViricimo/MyConfiguration.java
@@ -6,9 +6,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MyConfiguration {
 
-    @Bean(name = "applicationName") //Anotação @Bean cria o método no contexto da aplicação e poderá ser utilizado em qualquer parte da aplicação e se não for passado um nome para o bean o nome será o próprio nome do método;
+    /*@Bean(name = "applicationName") //Anotação @Bean cria o método no contexto da aplicação e poderá ser utilizado em qualquer parte da aplicação e se não for passado um nome para o bean o nome será o próprio nome do método;
     public String applicationName(){
         return "Sistema de Vendas";
     }
+
+     */
 
 }

081b33fc9e5b44a6e2c4167b98a92e5198c33d84
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-07-30 21:50:03 -0300

    Comentários injeção de dependência

--- src/main/java/repository/MyRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository //Indicando para o sprint que a classe irá acessar a base de dados
-public class MyRepository {
-}

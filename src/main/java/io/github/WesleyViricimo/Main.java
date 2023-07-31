a3422cd8ef16973d2a228db5870ee9b9adcb4419
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-07-31 15:53:16 -0300

    Trabalhando com perfis de desenvolvimento

--- src/main/java/io/github/WesleyViricimo/Main.java
+++ src/main/java/io/github/WesleyViricimo/Main.java
@@ -1,12 +1,11 @@
 package io.github.WesleyViricimo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @ComponentScan(basePackages = {"io.github.WesleyViricimo.repository", "io.github.WesleyViricimo.repository"}) //Anotação para indicar para o spring quais são os componentes, classes de configurações e objetos que deverão ser escaneados, mas o spring já escaneia todos os pacotes que estão no pacote principal e é mais utilizada esta anotação quando utilizadas bibliotecas de terceiros
 @SpringBootApplication //Anotação para Spring reconhecer que a classe irá inicializar uma aplicação Spring Boot
--- src/main/java/io/github/WesleyViricimo/MyConfiguration.java
+++ src/main/java/io/github/WesleyViricimo/MyConfiguration.java
@@ -1,16 +1,19 @@
 package io.github.WesleyViricimo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MyConfiguration {
 
-    /*@Bean(name = "applicationName") //Anotação @Bean cria o método no contexto da aplicação e poderá ser utilizado em qualquer parte da aplicação e se não for passado um nome para o bean o nome será o próprio nome do método;
-    public String applicationName(){
-        return "Sistema de Vendas";
+    @Bean(name = "applicationName") //Anotação @Bean cria o método no contexto da aplicação e poderá ser utilizado em qualquer parte da aplicação e se não for passado um nome para o bean o nome será o próprio nome do método;
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("Executando a configuração de desenvolvimento");
+        };
     }
-
-     */
 
 }

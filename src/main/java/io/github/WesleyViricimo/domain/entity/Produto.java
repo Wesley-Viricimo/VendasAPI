38a79427936b58a38f54ec64b42ee32bb2b36f17
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-08-02 23:20:25 -0300

    Mapeando entidade produto

--- src/main/java/io/github/WesleyViricimo/domain/entity/Produto.java
+++ src/main/java/io/github/WesleyViricimo/domain/entity/Produto.java
@@ -1,11 +1,21 @@
 package io.github.WesleyViricimo.domain.entity;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "T_PRODUTO")
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "valor_produto")
     private BigDecimal valorProduto;
 
     public Integer getId() {

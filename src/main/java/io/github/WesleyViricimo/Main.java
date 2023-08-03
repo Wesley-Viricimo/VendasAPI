f2b8487eeb327d6ac0feb3048d365f8ddb1049f2
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-08-02 22:18:13 -0300

    Anotação @Query para realizar consultas

--- src/main/java/io/github/WesleyViricimo/Main.java
+++ src/main/java/io/github/WesleyViricimo/Main.java
@@ -21,34 +21,8 @@ public class Main {
             clientes.save(new Cliente("Jessica"));
             clientes.save(new Cliente("Ana Julia"));
 
-            List<Cliente> todosClientes = clientes.findAll();
-            todosClientes.forEach(System.out::println);
-
-            System.out.println("Atualizando clientes");
-            todosClientes.forEach(c -> { //Para cada elemento da lista
-                c.setNome(c.getNome() + " atualizado!");
-                clientes.save(c);
-                System.out.println(c);
-            });
-
-            System.out.println("Buscando clientes por nome");
-            clientes.findByNomeLike("Wesley").forEach(System.out::println);
-
-            boolean existe = clientes.existsByNome("Wesley atualizado!");
-            System.out.println("Existe por nome " + existe);
-
-            System.out.println("Deletando clientes");
-            clientes.findAll().forEach(c -> {
-                clientes.delete(c);
-            });
-
-            todosClientes = clientes.findAll();
-
-            if(todosClientes.isEmpty()){
-                System.out.println("Nenhum cliente encontrado!");
-            } else {
-                todosClientes.forEach(System.out::println);
-            }
+            List<Cliente> result = clientes.encontrarPorNome("Wesley");
+            result.forEach(System.out::println);
 
         };
     }
--- src/main/java/io/github/WesleyViricimo/domain/repository/ClienteRepository.java
+++ src/main/java/io/github/WesleyViricimo/domain/repository/ClienteRepository.java
@@ -2,10 +2,20 @@ package io.github.WesleyViricimo.domain.repository;
 
 import io.github.WesleyViricimo.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-    List<Cliente> findByNomeLike(String nome); //Para a utilização do spring data JPA, deve ser extendida da classe JpaRepository e deve ser informada a classe entidade e o tipo da variável que é o ID
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> { //Para a utilização do spring data JPA, deve ser extendida da classe JpaRepository e deve ser informada a classe entidade e o tipo da variável que é o ID
+
+    @Query(value = " select * from t_cliente c where c.nome like '%:nome%' ", nativeQuery = true)//Utilizando @Query para realizar consultas com sql nativo, no value adicionar a consulta e no nativeQuery setar o valor true
+    List<Cliente> encontrarPorNome(@Param("nome") String nome); //Deverá ser adicionada a anotação param caso for recebido algum parâmetro
+
+    @Query(value = " delete from t_cliente where nome = ':nome' ", nativeQuery = true)
+    @Modifying //Precisa adicionar anotação modifying para utilizar a anotação @Query para métodos de crud
+    void deleteByNome(String nome);
 
     boolean existsByNome(String nome);//QueryMethod para retornar se existe um cliente com o nome que foi recebido como parâmetro
 

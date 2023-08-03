640bcc455ee0593b636f94ded244dbe49fd29b1c
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-08-03 18:32:38 -0300

    Funcionalidades do request mapping

--- src/main/java/io/github/WesleyViricimo/rest/controller/ClienteController.java
+++ src/main/java/io/github/WesleyViricimo/rest/controller/ClienteController.java
@@ -1,18 +1,19 @@
 package io.github.WesleyViricimo.rest.controller;
 
+import io.github.WesleyViricimo.domain.entity.Cliente;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller //Definindo que a classe será uma classe de controle de requisições rest
 @RequestMapping("/api/clientes")//Quando o usuário acessar este endpoint irá acessar os métodos desta classe
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @RequestMapping(value = {"/hello/{nome}", "/api/hello"},
+            method = RequestMethod.POST,
+            consumes = {"application/json", "application/xml"}//Consumes permite que o usuário envie um json ou xml no corpo da requisição
+            )
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente) {
+    public String helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente /*Recebendo o json ou xml do corpo da requisição e transformando em objeto*/) {
         return String.format("Hello %s ", nomeCliente);
     }
 }

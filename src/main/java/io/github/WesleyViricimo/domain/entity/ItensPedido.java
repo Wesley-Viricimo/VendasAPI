a72b54ba832551e448d697d70df40a0b757f6c66
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-08-02 23:53:32 -0300

    Mapeando entidade de itens do pedido

--- src/main/java/io/github/WesleyViricimo/domain/entity/ItensPedido.java
+++ src/main/java/io/github/WesleyViricimo/domain/entity/ItensPedido.java
@@ -1,10 +1,25 @@
 package io.github.WesleyViricimo.domain.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "T_ITENS_PEDIDO")
 public class ItensPedido {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
     private Integer id;
+
+    @ManyToOne//Anotação muitos para um, neste caso muitos itens de pedido podem estar em apenas um pedido
+    @JoinColumn(name = "PEDIDO_ID")
     private Pedido pedido;
+
+    @ManyToOne//Anotação muitos para um, neste caso muitos itens de pedido podem estar em um produto
+    @JoinColumn(name = "PRODUTO_ID")
     private Produto produto;
+
+    @Column(name = "QUANTIDADE")
     private Integer quantidade;
 
     public Integer getId() {
--- src/main/java/io/github/WesleyViricimo/domain/entity/Pedido.java
+++ src/main/java/io/github/WesleyViricimo/domain/entity/Pedido.java
@@ -3,6 +3,7 @@ package io.github.WesleyViricimo.domain.entity;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Entity
 @Table(name = "T_PEDIDO")
@@ -22,4 +23,47 @@ public class Pedido {
 
     @Column(name = "VALOR_PEDIDO", length = 20, precision = 2)//Coluna terá o tamanho de 20 caracteres com 2 casas decimais
     private BigDecimal valorPedido;
+
+    @OneToMany(mappedBy = "pedido")//Relacionamento de um para muitos, neste caso um pedido poderá ter muitos itens do pedido (mapped by deverá ser passado o atributo que representa este relacionamento)
+    private Set<ItensPedido> itensPedidos;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDate getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(LocalDate dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+
+    public BigDecimal getValorPedido() {
+        return valorPedido;
+    }
+
+    public void setValorPedido(BigDecimal valorPedido) {
+        this.valorPedido = valorPedido;
+    }
+
+    public Set<ItensPedido> getItensPedidos() {
+        return itensPedidos;
+    }
+
+    public void setItensPedidos(Set<ItensPedido> itensPedidos) {
+        this.itensPedidos = itensPedidos;
+    }
 }

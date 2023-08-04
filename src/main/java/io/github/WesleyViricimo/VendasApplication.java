00c9afe093d3da8cc3ad09db80927d35f3f2bd4d
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-08-04 11:12:30 -0300

    Implementação da busca de cliente por id

--- src/main/java/io/github/WesleyViricimo/VendasApplication.java
+++ src/main/java/io/github/WesleyViricimo/VendasApplication.java
@@ -1,11 +1,24 @@
 package io.github.WesleyViricimo;
 
+import io.github.WesleyViricimo.domain.entity.Cliente;
+import io.github.WesleyViricimo.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication //Anotação para Spring reconhecer que a classe irá inicializar uma aplicação Spring Boot
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired ClienteRepository clientes){
+        return args -> {
+            Cliente c = new Cliente(null, "Wesley");
+            clientes.save(c);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
--- src/main/java/io/github/WesleyViricimo/rest/controller/ClienteController.java
+++ src/main/java/io/github/WesleyViricimo/rest/controller/ClienteController.java
@@ -1,19 +1,29 @@
 package io.github.WesleyViricimo.rest.controller;
 
 import io.github.WesleyViricimo.domain.entity.Cliente;
+import io.github.WesleyViricimo.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller //Definindo que a classe será uma classe de controle de requisições rest
 @RequestMapping("/api/clientes")//Quando o usuário acessar este endpoint irá acessar os métodos desta classe
 public class ClienteController {
 
-    @RequestMapping(value = {"/hello/{nome}", "/api/hello"},
-            method = RequestMethod.POST,
-            consumes = {"application/json", "application/xml"}//Consumes permite que o usuário envie um json ou xml no corpo da requisição
-            )
+    @Autowired
+    private ClienteRepository repository;
+
+    @GetMapping(value = "/{id}")
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente /*Recebendo o json ou xml do corpo da requisição e transformando em objeto*/) {
-        return String.format("Hello %s ", nomeCliente);
+    public ResponseEntity getClienteById(@PathVariable Integer id){
+        Optional<Cliente> cliente = repository.findById(id);
+
+        if(cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }

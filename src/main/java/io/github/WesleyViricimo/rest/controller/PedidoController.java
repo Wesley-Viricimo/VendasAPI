964a6e1ae892365ec41485d9e8fb386bb6baa7b2
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-08-06 14:37:47 -0300

    Adicionando dependência spring boot dev tools

--- src/main/java/io/github/WesleyViricimo/rest/controller/PedidoController.java
+++ src/main/java/io/github/WesleyViricimo/rest/controller/PedidoController.java
@@ -1,11 +1,14 @@
 package io.github.WesleyViricimo.rest.controller;
 
 import io.github.WesleyViricimo.domain.entity.Pedido;
+import io.github.WesleyViricimo.domain.repository.PedidoRepository;
 import io.github.WesleyViricimo.rest.dto.PedidoDTO;
 import io.github.WesleyViricimo.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/pedidos")
@@ -13,6 +16,9 @@ public class PedidoController {
 
     @Autowired
     private PedidoService service;
+
+    @Autowired
+    private PedidoRepository repository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)

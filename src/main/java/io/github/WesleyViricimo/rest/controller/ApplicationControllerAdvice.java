2d5c63fb23fc64fb442159beec0a3cc85ebd7740
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-08-07 17:33:22 -0300

    Implementando exception customizado

---
+++ src/main/java/io/github/WesleyViricimo/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,20 @@
+package io.github.WesleyViricimo.rest.controller;
+
+import io.github.WesleyViricimo.exception.RegraNegocioException;
+import io.github.WesleyViricimo.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class) //Anotação ExceptionHandler marca o método para ser um tratador de exceções
+    @ResponseStatus(HttpStatus.BAD_REQUEST) //Quando este método for chamado irá retornar o status badRequest
+
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+}

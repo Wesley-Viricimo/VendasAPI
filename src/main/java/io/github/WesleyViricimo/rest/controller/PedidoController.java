521c34f5c92bf2150b4779abdcec9cfca6ae8c35
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-08-08 16:47:21 -0300

    Validando campo obrigatório da DTO de pedido utilizando bean validation

--- src/main/java/io/github/WesleyViricimo/rest/controller/PedidoController.java
+++ src/main/java/io/github/WesleyViricimo/rest/controller/PedidoController.java
@@ -15,6 +15,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
@@ -32,7 +33,7 @@ public class PedidoController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Integer save(@RequestBody PedidoDTO dto) { //Corpo da requisição de pedido será recebido na forma de DTO
+    public Integer save(@RequestBody @Valid PedidoDTO dto) { //Corpo da requisição de pedido será recebido na forma de DTO
         Pedido pedido = service.salvar(dto);
         return pedido.getId();
     }
--- src/main/java/io/github/WesleyViricimo/rest/dto/PedidoDTO.java
+++ src/main/java/io/github/WesleyViricimo/rest/dto/PedidoDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -11,7 +12,9 @@ import java.util.List;
 @AllArgsConstructor //Cria construtor com todos os argumentos
 @Data //Anotação que criará getters e setters automaticamente para as propriedades
 public class PedidoDTO {
+    @NotNull(message = "Informe o código do cliente!")
     private Integer cliente; //irá receber o id do cliente
+    @NotNull(message = "O Campo total do pedido é obrigatório!")
     private BigDecimal valorPedido; //Irá receber o valor total do pedido
     private List<ItensPedidoDTO> itens;
 }
